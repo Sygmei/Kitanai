ddb1a9fdaedc012709175f6ebfc9cc2954b7231f
Sygmei <Mizugola@gmail.com>
2016-11-03 17:24:18 +0100

    Update Kitanai.cpp

--- Kitanai.cpp
+++ Kitanai.cpp
@@ -207,9 +207,50 @@ TokenType::Type Token::getType()
 {
     return this->type;
 }
+void Token::setType(TokenType::Type type)
+{
+    this->type = type;
+}
 std::string Token::getValue()
 {
     return this->value;
+}
+void Token::addParameter(Token token)
+{
+    this->parameters.push_back(token);
+}
+std::vector<Token> Token::getParameters()
+{
+    return parameters;
+}
+void Token::inspect(int depth)
+{
+    for (int i = 0; i < depth; i++) std::cout << "    ";
+    std::cout << "- Token : " << TokenType::TypeDataR[type].first << ((value != "") ? "(" + value + ")" : "") << std::endl;
+    for (Token& childToken : parameters) {
+        childToken.inspect(depth + 1);
+    }
+}
+
+InstructionBloc::InstructionBloc()
+{
+
+}
+void InstructionBloc::addToken(Token token)
+{
+    this->instructions.push_back(token);
+}
+std::vector<Token>* InstructionBloc::getAllTokens()
+{
+    return &instructions;
+}
+void InstructionBloc::inspect(int depth)
+{
+    for (int i = 0; i < depth; i++) std::cout << "    ";
+    std::cout << "Instruction Bloc" << std::endl;
+    for (Token& rootToken : instructions) {
+        rootToken.inspect(depth + 1);
+    }
 }
 
 //Program
@@ -222,10 +263,10 @@ void Program::parseFile(std::string path)
     std::ifstream useFile;
     useFile.open(path);
     std::string currentLine;
+    std::vector<Token> instructionBuffer;
     if (useFile.is_open()) {
         std::cout << "Start Parsing File : " << path << std::endl;
         while (getline(useFile, currentLine)) {
-            std::vector<Token> currentInstruction;
             currentLine = replaceString(replaceString(currentLine, "	", "    "), "    ", "");
             if (currentLine != "") {
                 std::cout << ">> " << currentLine << std::endl;
@@ -235,13 +276,121 @@ void Program::parseFile(std::string path)
                 }
                 for (std::pair<int, int> parseInst : std::get<2>(cuttedString)) {
                     if (parseInst.first == 0) {
-                        currentInstruction.push_back(Token(TokenType::StaticString));
+                        instructionBuffer.push_back(Token(TokenType::StaticString, std::get<0>(cuttedString)[parseInst.second]));
                     }
                     else if (parseInst.first == 1) {
-                        stringToTokens(std::get<1>(cuttedString)[parseInst.second]);
+                        std::vector<Token> tokensFromCInst = stringToTokens(std::get<1>(cuttedString)[parseInst.second]);
+                        instructionBuffer.insert(instructionBuffer.end(), tokensFromCInst.begin(), tokensFromCInst.end());
                     }
                 }
             }      
         }
+    }
+    std::cout << "Buffering..." << std::endl;
+    int currentInstructionIndex = 0;
+    instructions.push_back(InstructionBloc());
+    for (Token tok : instructionBuffer) {
+        if (tok.getType() == TokenType::NewInstruction) {
+        }
+        else {
+            instructions[currentInstructionIndex].addToken(tok);
+        }
+    }
+
+    for (InstructionBloc& inst : instructions) {
+        bool blocChange = true;
+        std::cout << "Beginning instruction" << std::endl;
+        std::vector<Token*> compressionStack;
+        bool compressionStackOpened = false;
+        int compressionStackIndex = 0;
+        int stackType;
+        while (blocChange) {
+            //std::cout << "Bloc changed, restart" << std::endl;
+            blocChange = false;
+            int instIndex = 0;
+            while (instIndex < inst.getAllTokens()->size()) {
+                Token* tok = &inst.getAllTokens()->at(instIndex);
+                std::cout << "Current Token : " << TokenType::TypeDataR[tok->getType()].first << std::endl;
+                for (int it = 0; it < TokenType::TypeParameters.size(); it++) { //Type Parameters
+                    if (tok->getType() == TokenType::TypeParameters[it][0] && instIndex + TokenType::TypeParameters[it].size() - 1 < inst.getAllTokens()->size()) {
+                        //std::cout << "    @Type Parameters Inspection " << TokenType::TypeDataR[tok->getType()].first << std::endl;
+                        int pindex = 1;
+                        bool noAdd = true;
+                        std::vector<Token> toAdd;
+                        //std::cout << "VecSize " << TokenType::TypeParameters[it].size() << std::endl;
+                        //std::cout << "InsVecSize " << inst.getAllTokens()->size() << "/" << instIndex << std::endl;
+                        //std::cout << "Current Elem : " << TokenType::TypeDataR[inst.getAllTokens()->at(instIndex).getType()].first << " @" << instIndex << std::endl;
+                        for (int iPara = 1; iPara < TokenType::TypeParameters[it].size(); iPara++) {
+                            //std::cout << "Current iPara : " << iPara << std::endl;
+                            //std::cout << "Current ndIndex " << instIndex + pindex << std::endl;
+                            //std::cout << "        Expect : " << TokenType::TypeDataR[TokenType::TypeParameters[it][iPara]].first << std::endl;
+                            //std::cout << "        Got : " << TokenType::TypeDataR[inst.getAllTokens()->at(instIndex + pindex).getType()].first << std::endl;
+                            if (TokenType::TypeParameters[it][iPara] == inst.getAllTokens()->at(instIndex + pindex).getType()) {
+                                //std::cout << "        Add Parameter : " << TokenType::TypeDataR[inst.getAllTokens()->at(instIndex + pindex).getType()].first << std::endl;
+                                noAdd = false;
+                                toAdd.push_back(inst.getAllTokens()->at(instIndex + pindex));
+                                pindex++;
+                            }
+                            else {
+                                noAdd = true;
+                                break;
+                            }
+                        }
+                        //std::cout << "EOL" << std::endl;
+                        if (!noAdd) {
+                            for (int i = 0; i < toAdd.size(); i++) {
+                                //std::cout << "ADD : " << TokenType::TypeDataR[toAdd[i].getType()].first << std::endl; 
+                                tok->addParameter(toAdd[i]);
+                            }
+                            inst.getAllTokens()->erase(inst.getAllTokens()->begin() + instIndex + 1, inst.getAllTokens()->begin() + instIndex + toAdd.size() + 1);
+                            //std::cout << "BLOC CHANGED !" << std::endl;
+                            blocChange = true;
+                        }
+                    }
+                }
+                if (compressionStackOpened) {
+                    compressionStack.push_back(tok);
+                }
+                if (!blocChange) {
+                    for (int i = 0; i < TokenType::TypeCompression.size(); i++) { //Type compression 
+                        if (tok->getType() == TokenType::TypeCompression[i][0]) {
+                            std::cout << "    Stack opened for : " << TokenType::TypeDataR[tok->getType()].first << std::endl;
+                            compressionStackOpened = true;
+                            compressionStackIndex = instIndex;
+                            compressionStack.clear();
+                            stackType = i;
+                        }
+                        if (tok->getType() == TokenType::TypeCompression[i][1] && i == stackType) {
+                            compressionStack.pop_back();
+                            (*inst.getAllTokens())[instIndex] = Token(TokenType::TypeCompression[i][2]);
+                            tok = &inst.getAllTokens()->at(instIndex);
+                            for (Token* cstok : compressionStack) {
+                                std::cout << "      Add : " << TokenType::TypeDataR[cstok->getType()].first << std::endl;
+                                tok->addParameter(*cstok);
+                            }
+                            std::cout << "    Stack closed for : " << TokenType::TypeDataR[tok->getType()].first << std::endl;
+                            inst.getAllTokens()->erase(inst.getAllTokens()->begin() + compressionStackIndex, inst.getAllTokens()->begin() + instIndex);
+                            compressionStackOpened = false;
+                            compressionStackIndex = 0;
+                            compressionStack.clear();
+                            blocChange = true;
+                            stackType = 0;
+                        }
+                        else if (tok->getType() == TokenType::TypeCompression[i][1] && i != stackType) {
+                            std::cout << "ERROR STACK CROSSING : " << std::endl;
+                        }
+                    }
+                }
+                else {
+                    compressionStackOpened = false;
+                    compressionStack.clear();
+                    compressionStackIndex = 0;
+                }
+                instIndex++;
+            }
+        }
+    }
+    for (InstructionBloc& inst : instructions) {
+        inst.inspect();
     }
 }
