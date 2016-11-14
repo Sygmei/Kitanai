1aabc5ba87089a0ee57b1f16d22abf0f0ad98d60
Sygmei <Mizugola@gmail.com>
2016-11-14 18:18:58 +0100

    Update Kitanai.hpp

--- Kitanai.hpp
+++ Kitanai.hpp
@@ -7,6 +7,7 @@
 #include <typeinfo>
 #include <fstream>
 #include <algorithm>
+#include <functional>
 
 std::vector<std::string> split(const std::string &str, const std::string &delimiters = " ");
 std::string join(std::vector<std::string>& vector, std::string sep = "", int start = 0, int end = 0);
@@ -92,8 +93,8 @@ namespace TokenType
         OpenInstruction,
         CloseInstruction,
         Instruction,
-        StaticString,
-        StaticNumber,
+        String,
+        Number,
         Condition,
         OpenStackAccess,
         CloseStackAccess,
@@ -107,14 +108,13 @@ namespace TokenType
         Goto,
         Origin,
         NewInstruction,
-        LeftParenthesis,
-        RightParenthesis,
-        Parenthesis
+        End,
+        Null
     };
 
     static std::map<std::string, std::pair<std::string, Type>> TypeDataK = {
-        {"{", {"OpenInstruction", OpenInstruction}},
-        {"}", {"CloseInstruction", CloseInstruction}},
+        {"(", {"OpenInstruction", OpenInstruction}},
+        {")", {"CloseInstruction", CloseInstruction}},
         {"[", {"OpenStackAccess", OpenStackAccess}},
         {"]", {"CloseStackAccess", CloseStackAccess}},
         {"?", {"Condition", Condition}},
@@ -126,12 +126,11 @@ namespace TokenType
         {"&", {"Goto", Goto}},
         {"~", {"Origin", Origin}},
         {";", {"NewInstruction", NewInstruction}},
-        {"(", {"LeftParenthesis", LeftParenthesis}},
-        {")", {"RightParenthesis", RightParenthesis}}
+        {"%", {"End", End}}
     };
     static std::map<Type, std::pair<std::string, std::string>> TypeDataR = {
-        {OpenInstruction, {"OpenInstruction", "{"}},
-        {CloseInstruction, {"CloseInstruction", "}"}},
+        {OpenInstruction, {"OpenInstruction", "("}},
+        {CloseInstruction, {"CloseInstruction", ")"}},
         {OpenStackAccess, {"OpenStackAccess", "["}},
         {CloseStackAccess, {"CloseStackAccess", "]"}},
         {Condition, {"Condition", "?"}},
@@ -143,76 +142,176 @@ namespace TokenType
         {Goto, {"Goto", "&"}},
         {Origin, {"Origin", "~"}},
         {NewInstruction, {"NewInstruction", ";"}},
-        {LeftParenthesis, {"LeftParenthesis", "("}},
-        {RightParenthesis, {"RightParenthesis", ")"}},
+        {End, {"End", "%"}},
         {Function, {"Function", ""}},
-        {StaticString, {"StaticString", ""}},
-        {StaticNumber, {"StaticNumber", ""}},
+        {String, {"String", ""}},
+        {Number, {"Number", ""}},
         {Instruction, {"Instruction", ""}},
-        {Parenthesis, {"Parenthesis", ""}},
         {StackAccess, {"StackAccess", ""}},
+        {Null, {"Null", ""}}
     };
     static std::vector<std::vector<Type>> TypeParameters = {
         {Goto, Origin},
-        {Goto, StaticNumber},
-        {StackAt, StaticNumber},
+        {Goto, Number},
+        {StackAt, String},
+        {StackAt, Number},
         {Condition, Instruction, Instruction, Instruction},
         {Condition, Instruction, Instruction}
     };
     static std::vector<std::vector<Type>> TypeCompression = {
         {OpenInstruction, CloseInstruction, Instruction},
         {OpenStackAccess, CloseStackAccess, StackAccess},
-        {LeftParenthesis, RightParenthesis, Parenthesis}
-    };
-    static std::map<std::string, std::pair<std::string, Type>, cmpByStringLength> TypeDataF = {
-        {"print", {"print", Condition}}
     };
 }
 
+class Program;
 class Token
 {
     private:
         TokenType::Type type;
         std::string value;
-        std::vector<Token> parameters;
+        std::vector<Token*> parameters;
     public:
         Token(TokenType::Type type);
         Token(TokenType::Type type, std::string value);
         TokenType::Type getType();
+        std::string getPrintableType();
         void setType(TokenType::Type type);
         std::string getValue();
-        void addParameter(Token token);
-        std::vector<Token> getParameters();
+        void addParameter(Token* token);
+        std::vector<Token*>* getParameters();
         void inspect(int depth = 0);
+        void execute(Program* prg);
 };
-std::vector<Token> stringToTokens(std::string string);
+void parametersAffectation(std::vector<Token*>* tokens);
+std::vector<Token> stringToTokens(int& flagCounter, std::string string);
 
-class InstructionBloc
+namespace StdLib
 {
-    private:
-        std::vector<Token> instructions;
-    public:
-        InstructionBloc();
-        void addToken(Token token);
-        std::vector<Token>* getAllTokens();
-        void inspect(int depth = 0);
-};
-void inspectToken(Token& token);
-void inspectBloc(InstructionBloc& bloc);
+    Token* f_add(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        Token* b = tokens[1];
+        if (a->getType() == TokenType::Number && b->getType() == TokenType::Number)
+            return new Token(TokenType::Number, std::to_string(std::stoi(a->getValue()) + std::stoi(b->getValue())));
+        if (a->getType() == TokenType::String && b->getType() == TokenType::String)
+            return new Token(TokenType::String, a->getValue() + b->getValue());
+    }
+    Token* f_sub(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        Token* b = tokens[1];
+        if (a->getType() == TokenType::Number && b->getType() == TokenType::Number)
+            return new Token(TokenType::Number, std::to_string(std::stoi(a->getValue()) - std::stoi(b->getValue())));
+    }
+    Token* f_mul(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        Token* b = tokens[1];
+        if (a->getType() == TokenType::Number && b->getType() == TokenType::Number)
+            return new Token(TokenType::Number, std::to_string(std::stoi(a->getValue()) * std::stoi(b->getValue())));
+    }
+    Token* f_div(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        Token* b = tokens[1];
+        if (a->getType() == TokenType::Number && b->getType() == TokenType::Number)
+            return new Token(TokenType::Number, std::to_string(std::stoi(a->getValue()) / std::stoi(b->getValue())));
+    }
+    Token* f_eq(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        Token* b = tokens[1];
+        if (a->getType() == b->getType() && a->getValue() == b->getValue()) {
+            return new Token(TokenType::Number, "1");
+        } else {
+            return new Token(TokenType::Number, "0");
+        }
+    }
+    Token* f_neq(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        Token* b = tokens[1];
+        if (a->getType() == b->getType() && a->getValue() == b->getValue()) {
+            return new Token(TokenType::Number, "0");
+        } else {
+            return new Token(TokenType::Number, "1");
+        }
+    }
+    Token* f_gt(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        Token* b = tokens[1];
+    }
+    Token* f_ge(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        Token* b = tokens[1];
+    }
+    Token* f_lt(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        Token* b = tokens[1];
+    }
+    Token* f_le(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        Token* b = tokens[1];
+    }
+    Token* f_print(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        std::cout << "[Exe::print] " << a->getValue() << std::endl;
+    }
+    Token* f_input(std::vector<Token*> tokens) {
+        Token* a = tokens[0];
+        std::cout << "[Exe::input] " << a->getValue() << " : " << std::endl;
+        std::string userInput;
+        std::getline(std::cin, userInput);
+        return new Token(TokenType::String, userInput);
+    }
+    Token* f_int(std::vector<Token*> tokens) {
+
+    }
+    Token* f_random(std::vector<Token*> tokens) {
+        
+    }
+    Token* B_(std::string funcname, int amount, std::function<Token*(std::vector<Token*>)> func, std::vector<Token*> parameters) {
+        if (amount != parameters.size()) {
+            std::cout << "ERROR ! Number of parameters not correct in function : " << funcname << std::endl;
+            return nullptr;
+        }
+        return func(parameters);
+    }
+    std::pair<std::string, std::pair<std::function<Token*(std::vector<Token*>)>, int>> B__(std::string name, std::function<Token*(std::vector<Token*>)> func, int amount) {
+        std::pair<std::function<Token*(std::vector<Token*>)>, int> rpart = std::pair<std::function<Token*(std::vector<Token*>)>, int>(std::bind(B_, name, amount, func, std::placeholders::_1), amount);
+        return std::pair<std::string, std::pair<std::function<Token*(std::vector<Token*>)>, int>>(name, rpart);
+    }
+    static std::map<std::string, std::pair<std::function<Token*(std::vector<Token*>)>, int>> FunctionsName = {
+        B__("add", f_add, 2),
+        B__("sub", f_sub, 2),
+        B__("mul", f_mul, 2),
+        B__("div", f_div, 2),
+        B__("eq", f_eq, 2),
+        B__("neq", f_neq, 2),
+        B__("gt", f_gt, 2),
+        B__("ge", f_ge, 2),
+        B__("lt", f_lt, 2),
+        B__("le", f_le, 2),
+        B__("print", f_print, 1),
+        B__("input", f_input, 1),
+        B__("int", f_int, 1),
+        B__("random", f_random, 0)
+    };
+}
 
 class Program
 {
     private:
-        std::vector<InstructionBloc> instructions;
+        Token instructions = Token(TokenType::Instruction);
         std::map<std::string, int> flags;
         std::vector<std::string> origins;
-        std::vector<Attribute*> stack;
+        std::map<std::string, std::vector<Token*>> stack;
         std::vector<std::string> staticStrings;
-        int stackPosition;
+        std::string stackPosition = "";
+        int subStackPosition = 0;
         int linePosition;
     public:
         Program();
         void parseFile(std::string path);
+        void exec();
+        void setStackPosition(std::string pos);
+        void setStackPosition(int pos);
+        Token* getStackAt();
 };
 
 void execFile(std::string path);
