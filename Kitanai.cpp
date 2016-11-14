6fb292ef5c9d53595f499b222a327f1f10f7c239
Sygmei <Mizugola@gmail.com>
2016-11-14 18:18:32 +0100

    Update Kitanai.cpp

--- Kitanai.cpp
+++ Kitanai.cpp
@@ -122,7 +122,7 @@ bool isStringNumeric(std::string str)
     return (isStringFloat(str) || isStringInt(str));
 }
 
-std::vector<Token> stringToTokens(std::string string)
+std::vector<Token> stringToTokens(int& flagCounter, std::string string)
 {
     std::vector<Token> allTokens;
     std::vector<std::string> splittedString = split(string, " ");
@@ -130,7 +130,7 @@ std::vector<Token> stringToTokens(std::string string)
     for (std::string cstring : splittedString) {
         if (othStack != "") {
             if (isStringNumeric(othStack)) {
-                allTokens.push_back(Token(TokenType::StaticNumber, othStack));
+                allTokens.push_back(Token(TokenType::Number, othStack));
             }
             else {
                 allTokens.push_back(Token(TokenType::Function, othStack));
@@ -144,14 +144,19 @@ std::vector<Token> stringToTokens(std::string string)
                 if (currentChar == it.first) {
                     if (othStack != "") {
                         if (isStringNumeric(othStack)) {
-                            allTokens.push_back(Token(TokenType::StaticNumber, othStack));
+                            allTokens.push_back(Token(TokenType::Number, othStack));
                         }
                         else {
                             allTokens.push_back(Token(TokenType::Function, othStack));
                         }
                         othStack = "";
                     }
-                    allTokens.push_back(Token(it.second.second));
+                    if (it.second.second == TokenType::Flag) {
+                        allTokens.push_back(Token(it.second.second, std::to_string(flagCounter++)));
+                    }
+                    else {
+                        allTokens.push_back(Token(it.second.second));
+                    }
                     tokenFound = true;
                     break;
                 }
@@ -163,7 +168,7 @@ std::vector<Token> stringToTokens(std::string string)
     }
     if (othStack != "") {
         if (isStringNumeric(othStack)) {
-            allTokens.push_back(Token(TokenType::StaticNumber, othStack));
+            allTokens.push_back(Token(TokenType::Number, othStack));
         }
         else {
             allTokens.push_back(Token(TokenType::Function, othStack));
@@ -188,6 +193,7 @@ int main(int argc, char** argv)
         std::cout << "Current file : " << currentFile << std::endl;
         Program prog;
         prog.parseFile(currentFile);
+        prog.exec();
     }
     std::cout << "END" << std::endl;
     return 0;
@@ -207,6 +213,10 @@ TokenType::Type Token::getType()
 {
     return this->type;
 }
+std::string Token::getPrintableType()
+{
+    return TokenType::TypeDataR[this->getType()].first;
+}
 void Token::setType(TokenType::Type type)
 {
     this->type = type;
@@ -215,48 +225,61 @@ std::string Token::getValue()
 {
     return this->value;
 }
-void Token::addParameter(Token token)
+void Token::addParameter(Token* token)
 {
     this->parameters.push_back(token);
 }
-std::vector<Token> Token::getParameters()
+std::vector<Token*>* Token::getParameters()
 {
-    return parameters;
+    return &parameters;
 }
 void Token::inspect(int depth)
 {
     for (int i = 0; i < depth; i++) std::cout << "    ";
     std::cout << "- Token : " << TokenType::TypeDataR[type].first << ((value != "") ? "(" + value + ")" : "") << std::endl;
-    for (Token& childToken : parameters) {
-        childToken.inspect(depth + 1);
+    for (Token* childToken : parameters) {
+        childToken->inspect(depth + 1);
     }
 }
-
-InstructionBloc::InstructionBloc()
+void Token::execute(Program* prg)
 {
-
-}
-void InstructionBloc::addToken(Token token)
-{
-    this->instructions.push_back(token);
-}
-std::vector<Token>* InstructionBloc::getAllTokens()
-{
-    return &instructions;
-}
-void InstructionBloc::inspect(int depth)
-{
-    for (int i = 0; i < depth; i++) std::cout << "    ";
-    std::cout << "Instruction Bloc" << std::endl;
-    for (Token& rootToken : instructions) {
-        rootToken.inspect(depth + 1);
+    for (Token* tok : parameters) {
+        tok->execute(prg);
     }
+    std::cout << "Execute Token : " << getPrintableType() << " : " << getValue() << std::endl;
+    if (getType() == TokenType::Function) {
+        StdLib::FunctionsName[getValue()].first(*getParameters());
+    }
+    else if (getType() == TokenType::StackAt) {
+        if (parameters[0]->getType() == TokenType::String) {
+            prg->setStackPosition(parameters[0]->getValue());
+        }
+        else if (parameters[0]->getType() == TokenType::String) {
+            prg->setStackPosition(std::stoi(parameters[0]->getValue()));
+        }
+    }
+    std::cin.get();
 }
+
 
 //Program
 Program::Program()
 {
 
+}
+void Program::setStackPosition(std::string pos)
+{
+    std::cout << "[Exe] StackPosition switched to : " << pos << std::endl;
+    stackPosition = pos;
+}
+void Program::setStackPosition(int pos)
+{
+    std::cout << "[Exe] SubStackPosition switched to : " << pos << std::endl;
+    subStackPosition = pos;
+}
+Token* Program::getStackAt()
+{
+    return stack[stackPosition][subStackPosition];
 }
 void Program::parseFile(std::string path)
 {
@@ -264,6 +287,7 @@ void Program::parseFile(std::string path)
     useFile.open(path);
     std::string currentLine;
     std::vector<Token> instructionBuffer;
+    int flagCounter = 0;
     if (useFile.is_open()) {
         std::cout << "Start Parsing File : " << path << std::endl;
         while (getline(useFile, currentLine)) {
@@ -276,10 +300,10 @@ void Program::parseFile(std::string path)
                 }
                 for (std::pair<int, int> parseInst : std::get<2>(cuttedString)) {
                     if (parseInst.first == 0) {
-                        instructionBuffer.push_back(Token(TokenType::StaticString, std::get<0>(cuttedString)[parseInst.second]));
+                        instructionBuffer.push_back(Token(TokenType::String, std::get<0>(cuttedString)[parseInst.second]));
                     }
                     else if (parseInst.first == 1) {
-                        std::vector<Token> tokensFromCInst = stringToTokens(std::get<1>(cuttedString)[parseInst.second]);
+                        std::vector<Token> tokensFromCInst = stringToTokens(flagCounter, std::get<1>(cuttedString)[parseInst.second]);
                         instructionBuffer.insert(instructionBuffer.end(), tokensFromCInst.begin(), tokensFromCInst.end());
                     }
                 }
@@ -288,16 +312,11 @@ void Program::parseFile(std::string path)
     }
     std::cout << "Buffering..." << std::endl;
     int currentInstructionIndex = 0;
-    instructions.push_back(InstructionBloc());
     for (Token tok : instructionBuffer) {
-        if (tok.getType() == TokenType::NewInstruction) {
-        }
-        else {
-            instructions[currentInstructionIndex].addToken(tok);
-        }
+        instructions.addParameter(new Token(tok));
     }
 
-    for (InstructionBloc& inst : instructions) {
+    for (Token* inst : *instructions.getParameters()) {
         bool blocChange = true;
         std::cout << "Beginning instruction" << std::endl;
         std::vector<Token*> compressionStack;
@@ -308,15 +327,15 @@ void Program::parseFile(std::string path)
             //std::cout << "Bloc changed, restart" << std::endl;
             blocChange = false;
             int instIndex = 0;
-            while (instIndex < inst.getAllTokens()->size()) {
-                Token* tok = &inst.getAllTokens()->at(instIndex);
+            while (instIndex < inst->getParameters()->size()) {
+                Token* tok = inst->getParameters()->at(instIndex);
                 std::cout << "Current Token : " << TokenType::TypeDataR[tok->getType()].first << std::endl;
                 for (int it = 0; it < TokenType::TypeParameters.size(); it++) { //Type Parameters
-                    if (tok->getType() == TokenType::TypeParameters[it][0] && instIndex + TokenType::TypeParameters[it].size() - 1 < inst.getAllTokens()->size()) {
+                    if (tok->getType() == TokenType::TypeParameters[it][0] && instIndex + TokenType::TypeParameters[it].size() - 1 < inst->getParameters()->size()) {
                         //std::cout << "    @Type Parameters Inspection " << TokenType::TypeDataR[tok->getType()].first << std::endl;
                         int pindex = 1;
                         bool noAdd = true;
-                        std::vector<Token> toAdd;
+                        std::vector<Token*> toAdd;
                         //std::cout << "VecSize " << TokenType::TypeParameters[it].size() << std::endl;
                         //std::cout << "InsVecSize " << inst.getAllTokens()->size() << "/" << instIndex << std::endl;
                         //std::cout << "Current Elem : " << TokenType::TypeDataR[inst.getAllTokens()->at(instIndex).getType()].first << " @" << instIndex << std::endl;
@@ -325,10 +344,10 @@ void Program::parseFile(std::string path)
                             //std::cout << "Current ndIndex " << instIndex + pindex << std::endl;
                             //std::cout << "        Expect : " << TokenType::TypeDataR[TokenType::TypeParameters[it][iPara]].first << std::endl;
                             //std::cout << "        Got : " << TokenType::TypeDataR[inst.getAllTokens()->at(instIndex + pindex).getType()].first << std::endl;
-                            if (TokenType::TypeParameters[it][iPara] == inst.getAllTokens()->at(instIndex + pindex).getType()) {
+                            if (TokenType::TypeParameters[it][iPara] == inst->getParameters()->at(instIndex + pindex)->getType()) {
                                 //std::cout << "        Add Parameter : " << TokenType::TypeDataR[inst.getAllTokens()->at(instIndex + pindex).getType()].first << std::endl;
                                 noAdd = false;
-                                toAdd.push_back(inst.getAllTokens()->at(instIndex + pindex));
+                                toAdd.push_back(inst->getParameters()->at(instIndex + pindex));
                                 pindex++;
                             }
                             else {
@@ -342,7 +361,7 @@ void Program::parseFile(std::string path)
                                 //std::cout << "ADD : " << TokenType::TypeDataR[toAdd[i].getType()].first << std::endl; 
                                 tok->addParameter(toAdd[i]);
                             }
-                            inst.getAllTokens()->erase(inst.getAllTokens()->begin() + instIndex + 1, inst.getAllTokens()->begin() + instIndex + toAdd.size() + 1);
+                            inst->getParameters()->erase(inst->getParameters()->begin() + instIndex + 1, inst->getParameters()->begin() + instIndex + toAdd.size() + 1);
                             //std::cout << "BLOC CHANGED !" << std::endl;
                             blocChange = true;
                         }
@@ -362,14 +381,14 @@ void Program::parseFile(std::string path)
                         }
                         if (tok->getType() == TokenType::TypeCompression[i][1] && i == stackType) {
                             compressionStack.pop_back();
-                            (*inst.getAllTokens())[instIndex] = Token(TokenType::TypeCompression[i][2]);
-                            tok = &inst.getAllTokens()->at(instIndex);
+                            (*inst->getParameters())[instIndex] = new Token(TokenType::TypeCompression[i][2]);
+                            tok = inst->getParameters()->at(instIndex);
                             for (Token* cstok : compressionStack) {
                                 std::cout << "      Add : " << TokenType::TypeDataR[cstok->getType()].first << std::endl;
-                                tok->addParameter(*cstok);
+                                tok->addParameter(new Token(*cstok));
                             }
                             std::cout << "    Stack closed for : " << TokenType::TypeDataR[tok->getType()].first << std::endl;
-                            inst.getAllTokens()->erase(inst.getAllTokens()->begin() + compressionStackIndex, inst.getAllTokens()->begin() + instIndex);
+                            inst->getParameters()->erase(inst->getParameters()->begin() + compressionStackIndex, inst->getParameters()->begin() + instIndex);
                             compressionStackOpened = false;
                             compressionStackIndex = 0;
                             compressionStack.clear();
@@ -390,7 +409,41 @@ void Program::parseFile(std::string path)
             }
         }
     }
-    for (InstructionBloc& inst : instructions) {
-        inst.inspect();
+    //Function Imbrication
+    for (Token* ins : *instructions.getParameters()) {
+        parametersAffectation(ins->getParameters());
+    }
+}
+void parametersAffectation(std::vector<Token*>* tokens)
+{
+    for (int i = 0; i < tokens->size(); i++) {
+        Token* tok = tokens->at(i);
+        parametersAffectation(tok->getParameters());
+        std::cout << tok->getPrintableType() << " // " << tok->getValue() << std::endl;
+        if (tok->getType() == TokenType::Function) {
+            std::cout << "Type Func" << std::endl;
+            std::vector<int> indexToErase;
+            for (int j = i + 1; j < StdLib::FunctionsName[tok->getValue()].second + i + 1; j++) {
+                std::cout << "[ADDING PARAMETER] : " << tokens->at(j)->getPrintableType() << std::endl;
+                tok->addParameter(tokens->at(j));
+                parametersAffectation(tokens->at(j)->getParameters());
+                indexToErase.push_back(j);
+                std::cout << "Closure" << std::endl;
+            }
+            std::reverse(indexToErase.begin(), indexToErase.end());
+            for (int j = 0; j < indexToErase.size(); j++) {
+                std::cout << "Er:/" << indexToErase[j] << "oOf+" << tokens->size() << "?" << j << std::endl;
+                tokens->erase(tokens->begin() + indexToErase[j]);
+            }
+        }
+    }
+}
+void Program::exec()
+{
+    for (Token* ins : *instructions.getParameters()) {
+        ins->inspect();
+    }
+    for (Token* ins : *instructions.getParameters()) {
+        ins->execute(this);
     }
 }
